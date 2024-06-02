24494f07ec1f444469c032606306b0d339472352
Fabian Mahdi <fabian@mahdi.at>
2024-06-02 22:12:45 +0200

    added @DirtiesContext annotation so that tests dont hang after running

--- src/test/java/Ci/Backend/Repositories/RoleRepositoryTest.java
+++ src/test/java/Ci/Backend/Repositories/RoleRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext
 @IntegrationTest
 class RoleRepositoryTest
 {
--- src/test/java/Ci/Backend/Repositories/UserRepositoryTest.java
+++ src/test/java/Ci/Backend/Repositories/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package Ci.Backend.Repositories;
 
+import Ci.Backend.Categories.IntegrationTest;
 import Ci.Backend.Models.UserEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,6 +26,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext
+@IntegrationTest
 class UserRepositoryTest
 {
     @Container
